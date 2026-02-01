53eaa1578e2d1ad59728e4b9d369242db9808cb1
kallel-dev <kallelpr@gmail.com>
2026-02-01 15:43:52 +0000

    nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 //Estou montando a carta com printf
+//Fazendo agora alterações para nível aventureiro
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
@@ -8,12 +9,14 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
-    char cidade[50];
+     char cidade[50];
     int codigo;
     float populacao;
     float area;
     float pib;
     int pontos;
+    float densidadepop;
+    float pibpercapita;
 
     char cidade2[50];
     int codigo2;
@@ -21,77 +24,69 @@ int main() {
     float area2;
     float pib2;
     int pontos2;
-
-
-
-    
-  // Área para entrada de dados
+    float densidadepop2;
+    float pibpercapita2;
 
     // Cidade 1
     printf("Digite o nome da cidade que ira representar Tocantins: ");
     scanf("%s", cidade);
-
     printf("Digite o código de 1 a 4: ");
     scanf("%d", &codigo);
-
     printf("Digite a populacao da sua cidade: ");
     scanf("%f", &populacao);
-
     printf("Digite a área aproximada da cidade em km2: ");
     scanf("%f", &area);
-
     printf("Digite o PIB aproximado da cidade: ");
     scanf("%f", &pib);
-
     printf("Digite a quantidade de pontos turisticos da cidade: ");
     scanf("%d", &pontos);
-
     printf("\n");
+
+    // Calculando densidade e PIB per capita da cidade 1
+    densidadepop = populacao / area;
+    pibpercapita = pib / populacao;
 
     // Cidade 2
     printf("Digite o nome da segunda cidade que ira representar Tocantins: ");
     scanf("%s", cidade2);
-
-    printf("Digite o código (diferente de 1 a 4): ");
+    printf("Digite o código (1 a 4): ");
     scanf("%d", &codigo2);
-
     printf("Digite a populacao da cidade: ");
     scanf("%f", &populacao2);
-
     printf("Digite a área aproximada da cidade em km2: ");
     scanf("%f", &area2);
-
     printf("Digite o PIB aproximado da cidade: ");
     scanf("%f", &pib2);
-
     printf("Digite a quantidade de pontos turisticos da cidade: ");
     scanf("%d", &pontos2);
-
     printf("\n");
 
-
-  // Área para exibição dos dados da cidade
+    // Calculando densidade e PIB per capita da cidade 2
+    densidadepop2 = populacao2 / area2;
+    pibpercapita2 = pib2 / populacao2;
 
     // Primeira Carta
     printf("Carta 01\n");
     printf("Cidade: %s\n", cidade);
-    printf("Codigo da Cidade: T%d\n", codigo);
+    printf("Codigo da Cidade: A%d\n", codigo);
     printf("Populacao: %.0f habitantes\n", populacao);
     printf("Area: %.2f km2\n", area);
-    printf("PIB: %.2f bilhões\n", pib);
+    printf("PIB: %.2f\n", pib);
     printf("Pontos Turisticos: %d\n", pontos);
-
+    printf("Densidade Populacional em número de habitantes por Km2: %.2f\n", densidadepop);
+    printf("O Pib per capita é: %.2f\n", pibpercapita);
     printf("\n");
 
     // Segunda Carta
     printf("Carta 02\n");
     printf("Cidade: %s\n", cidade2);
-    printf("Codigo da Cidade: T%d\n", codigo2);
+    printf("Codigo da Cidade: B%d\n", codigo2);
     printf("Populacao: %.0f habitantes\n", populacao2);
     printf("Area: %.2f km2\n", area2);
-    printf("PIB: %.2f bilhões\n", pib2);
+    printf("PIB: %.2f\n", pib2);
     printf("Pontos Turisticos: %d\n", pontos2);
-
-    
+    printf("Densidade Populacional em número de habitantes por Km2: %.2f\n", densidadepop2);
+    printf("O Pib per capita é: %.2f\n", pibpercapita2);
+//Nível Aventureiro Concluído
     return 0;
 }
